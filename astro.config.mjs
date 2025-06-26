424d3a97fd3d29ef05645e9ad4f41606520123b8
Pablo Portas López <81629707+TeenBiscuits@users.noreply.github.com>
2025-06-26 13:24:40 +0200

    Remove Cloudflare Wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: 'https://www.servidorgalletas.com',
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
